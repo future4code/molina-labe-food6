2b8b0c0e72af46daeaf4f7cda66e846c751baf8f
edersonamaral <ederson.rafael30@gmail.com>
2021-07-26 14:35:57 -0300

    criado rotas e coordenadas para futuras mudanças de telas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from 'react'
+// import Router from './Routes/Router'
+// import GlobalState from './Global/GlobalState'
 
 
 const App = () => {
   return (
     <div>
-    
+      {/* <GlobalState>
+        <Router />
+      </GlobalState>     */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Global/GlobalState.js
@@ -0,0 +1,13 @@
+import GlobalStateContext from './GlobalStateContext'
+import React from 'react'
+
+const GlobalState = () => {
+    return(
+        <div>
+            <GlobalStateContext>
+
+            </GlobalStateContext>
+        </div>
+    )
+}
+export default GlobalState
