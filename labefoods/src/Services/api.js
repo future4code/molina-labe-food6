cf86aebd6b769dc610d7187fd43869c09ab5b6a8
edersonamaral <ederson.rafael30@gmail.com>
2021-07-26 17:20:21 -0300

    instalado bibliotecas que serão usadas no projeto

--- labefoods/src/Services/api.js
+++ labefoods/src/Services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://us-central1-missao-newton.cloudfunctions.net/futureEatsA',
+    baseURL: 'https://us-central1-missao-newton.cloudfunctions.net/futureEatsB',
 })
